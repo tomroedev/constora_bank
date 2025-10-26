220dbe08b63c4b561dbac510c83e7d3ab21e5cfe
Tom Roe <tom.roe@live.co.uk>
2025-10-26 12:02:18 +0000

    Added ConstoraPage which is the high level Surface Composable with a Box composable that has a set Modifier with padding and content alignment that will be used across screens.

---
+++ app/src/main/java/com/example/constorabank/core/designsystem/components/ConstoraPage.kt
@@ -0,0 +1,31 @@
+package com.example.constorabank.core.designsystem.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ConstoraPage (
+    content: @Composable BoxScope.() -> Unit
+) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background,
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 32.dp),
+            contentAlignment = Alignment.TopCenter,
+        ) {
+            content()
+        }
+    }
+}
